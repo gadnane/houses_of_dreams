d23c3530c53d7c946e9c31369c4af5948f32b154
Adnane <ad.gasmi@gmail.com>
2014-11-25 15:30:13 -0500

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Houses of Dreams!"
+      log_in @user
+      flash[:success] = "Welcome to Houses of Dreams"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'signup' => 'users#new'
+
+  get    'login'   => 'sessions#new'
+
+  post   'login'   => 'sessions#create'
+  
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
