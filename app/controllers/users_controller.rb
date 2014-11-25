55eeab1050ec02d7b1a1458de3aeca1f07996a98
Adnane <ad.gasmi@gmail.com>
2014-11-25 14:30:35 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Houses of Dreams!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :firstname, :lastname, :maxrent, :password,
+                                   :password_confirmation)
+    end
+
 end
